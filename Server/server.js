b6ab4e46953f973a2f3cd105df959dff73810ffc
Anas Abbas <anasabbasmalik88@gmail.com>
2025-03-01 15:08:18 +0500

    Server Setup

---
+++ Server/server.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv')
+dotenv.config({path : './config.env'})
+ const app = require("./app");
+const port = process.env.PORT_NUMBER || 3000;
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
