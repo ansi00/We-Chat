35416d1a0ea0745d42837722c850b528499aca6e
Anas Abbas <anasabbasmalik88@gmail.com>
2025-03-04 14:48:58 +0500

    created chatSchema and functionality to create and get chats

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authRouter = require("./controllers/authControllers");
 const userRouter = require("./controllers/userController");
+const chatRouter = require("./controllers/chatController");
 const app = express();
 
 // authController router
@@ -9,5 +10,8 @@ app.use("/api/auth/", authRouter);
 
 //userController router
 app.use("/api/user/", userRouter);
+
+//chatController router
+app.use("/api/chat", chatRouter);
 
 module.exports = app;
---
+++ Server/models/chat.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema(
+  {
+    members: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Users",
+        },
+      ],
+      lastMessage: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Messages",
+      },
+      unreadMessageCount: {
+        type: Number,
+        default: 0,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const Chat = mongoose.model("Chat", chatSchema);
+module.exports = Chat;
